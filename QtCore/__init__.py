cb72a1c7d1ac08c5157700fc112ad9564eba82fd
5yutan5 <63651161+5yutan5@users.noreply.github.com>
2021-09-05 21:23:42 +0900

    Add Signal class wrapper of PyQt

--- QtCore/__init__.py
+++ QtCore/__init__.py
@@ -6,11 +6,15 @@ elif QT_API == "PyQt6":
     from PyQt6.QtCore import *
 
     Slot = pyqtSlot
+    Signal = pyqtSignal
     del pyqtSlot
+    del pyqtSignal
 elif QT_API == "PyQt5":
     from PyQt5.QtCore import *
 
     Slot = pyqtSlot
+    Signal = pyqtSignal
     del pyqtSlot
+    del pyqtSignal
 elif QT_API == "PySide2":
     from PySide2.QtCore import *
