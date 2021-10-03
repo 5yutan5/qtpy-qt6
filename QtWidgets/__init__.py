7ffae0d26cc35df296377d4fa6a199d733e7723a
5yutan5 <63651161+5yutan5@users.noreply.github.com>
2021-10-04 03:44:38 +0900

    Refactoring

--- QtWidgets/__init__.py
+++ QtWidgets/__init__.py
@@ -1,23 +1,27 @@
 from ..qt_compat import QT_API
+from ..QtCore import Qt
 
 if QT_API == "PySide6":
-    from PySide6.QtWidgets import *
+    from PySide6.QtWidgets import *  # noqa: F401, F403
 elif QT_API == "PyQt6":
-    from PyQt6.QtWidgets import *
+    from PyQt6.QtWidgets import *  # noqa: F401, F403
 elif QT_API == "PyQt5":
-    from PyQt5.QtWidgets import *
+    from PyQt5.QtWidgets import *  # noqa: F401, F403
 elif QT_API == "PySide2":
-    from PySide2.QtWidgets import *
+    from PySide2.QtWidgets import *  # noqa: F401, F403
 
 
-class Application(QApplication):
-    def __init__(self, arg__1) -> None:
-        super().__init__(arg__1)
-
+class Application(QApplication):  # noqa: F405
     def exec(self) -> int:
         if QT_API == "PySide2":
             return super().exec_()
         return super().exec()
+
+    def setStyleSheet(self, sheet: str) -> None:
+        return super().setStyleSheet(sheet)
+
+    def setAttribute(self, attribute: Qt.ApplicationAttribute, on: bool = True) -> None:
+        super().setAttribute(attribute, on)
 
 
 QApplication = Application
